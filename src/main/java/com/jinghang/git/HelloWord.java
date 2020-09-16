019b2c9df6ccc28d27f017a0787d1c53ec26f188
little <hs1546971480@163.com>
2020-09-16 17:16:35 +0800

    我的第二次提交

--- src/main/java/com/jinghang/git/HelloWord.java
+++ src/main/java/com/jinghang/git/HelloWord.java
@@ -3,6 +3,6 @@ package com.jinghang.git;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("这是我的第一次提交");
-        
+        System.out.println("这是我的第二次提交hotfix");
     }
 }
